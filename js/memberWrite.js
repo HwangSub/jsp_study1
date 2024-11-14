54bdb3bb4794502ace559f83c0e64ff5716e7284
HwangSub <48292796+HwangSub@users.noreply.github.com>
2024-11-14 14:55:18 +0900

    Add files via upload

--- js/memberWrite.js
+++ js/memberWrite.js
@@ -2,43 +2,62 @@
  * 
  */
 function fn_submit() {
-	 
-	if(document.frm.userid.value == "") {
+	
+	let f = document.frm;
+	
+	if(f.userid.value == "") {
 		alert("아이디를 입력해주세요.");	
-		document.frm.userid.focus();
+		f.userid.focus();
 		return false;
 	}
 	
-	if(document.frm.userid.value.length < 4 ||
-	   document.frm.userid.value.length > 12 ) 
+	if(f.userid.value.length < 4 ||
+	   f.userid.value.length > 12 ) 
 	{
 		alert("아이디는 4자리~12자리 사이로 입력해주세요.");
-		document.frm.userid.focus();
+		f.userid.focus();
 		return false;
 	}
 	
-	if(document.frm.pass.value == "") {
+	if(f.pass.value == "") {
 		alert("암호를 입력해주세요.");	
-		document.frm.pass.focus();
+		f.pass.focus();
 		return false;
 	}
 	
-	if(document.frm.pass.value.length < 4 ||
-	   document.frm.pass.value.length > 20 ) 
+	if(f.pass.value.length < 4 ||
+	   f.pass.value.length > 20 ) 
 	{
 		alert("암호는 4자리~20자리 사이로 입력해주세요.");
-		document.frm.pass.focus();
+		f.pass.focus();
 		return false;
 	}
 	
-	if(document.frm.pass.value != document.frm.pass2.value) {
+	if(f.pass.value != f.pass2.value) {
 		alert("암호가 암호확인과 일치하지 않습니다.");
-		document.frm.pass.focus();
+		f.pass.focus();
+		return false;
+	}
+	
+	if( f.name.value == "" ) {
+		alert("이름을 다시확인해 주세요.");
+		f.name.focus();
 		return false;
 	}
 
-	document.frm.submit();
- 
+	if( f.tel2.value.length != 4 || f.tel3.value.length != 4 ) {
+		alert("연락처를 다시확인해 주세요.");
+		f.tel2.focus();
+		return false;
+	}
+	
+	if( f.mail1.value == "" || f.mail2.value == "" ) {
+		alert("이메일을 다시확인해 주세요.");
+		f.mail1.focus();
+		return false;
+	}
+	
+	f.submit();
  }
  
 
